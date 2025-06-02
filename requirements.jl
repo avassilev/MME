73122d97aa16430f76ad03170569ea2e8245437e
Andrey Vassilev <avassilev@gmail.com>
2025-06-02 17:12:48 +0300

    Requirements file + additions to optimization notebook

---
+++ requirements.jl
@@ -0,0 +1,14 @@
+import Pkg
+
+# required
+Pkg.add("Tidier")
+Pkg.add("Plots")
+Pkg.add("LaTeXStrings")
+Pkg.add("ModelingToolkit")
+Pkg.add("DifferentialEquations")
+Pkg.add("Roots")
+Pkg.add("JuMP")
+Pkg.add("Ipopt")
+
+# optional
+Pkg.add("HTTP")
